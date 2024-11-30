c2a6d1dd4931935af04a090b5e038b380a7fa7cf
MAS-80 <andrej.makruschin@yandex.ru>
2024-11-30 12:31:58 +0300

    Create task2, template/second_task, class_template.html, func_template.html.

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import hallo, Hallo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hallo/', hallo),
+    path('bye/', Hallo.as_view())
 ]
