4de51a1ec49f189e4b2beeb7ea106f0d2fb601ac
MAS-80 <andrej.makruschin@yandex.ru>
2024-11-30 12:42:32 +0300

    second

---
+++ task2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def hallo(request):
+    return render(request, 'class_template.html')
+
+class Hallo(TemplateView):
+    template_name =('func_template.html')
